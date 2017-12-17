114dfb85fe0fc259d8d79ba3a72a7df2e75592be
Quentin Hibon <qh.public@yahoo.com>
2017-12-17 21:46:31 +0100

    add calcurse_convert.py

---
+++ calcurse_convert.py
@@ -0,0 +1,34 @@
+import re
+import dateutil.parser
+import datetime
+
+def next_day_from_str(date):
+    return dateutil.parser.parse(date) + datetime.timedelta(days=1)
+
+
+def convert_ics(infile, outfile):
+    with open(infile) as fs:
+        content = fs.readlines()
+
+    p = re.compile('DTSTART:[0-9]{8}$')
+    new_content = []
+    for line in content:
+        if p.match(line):
+            day = line.split(':')[1]
+            new_content.append('DTSTART;VALUE=DATE:' + day)
+
+            # not needed?
+            # next_day = next_day_from_str(day)
+            # new_line = 'DTEND;VALUE=DATE:' + next_day.strftime("%Y%m%d") + '\n'
+            # new_content.append(new_line)
+        else:
+            new_content.append(line)
+
+    with open(outfile, 'w') as fs:
+        fs.write(''.join(new_content))
+
+
+if __name__ == '__main__':
+    infile = 'calcurse_cal.ics'
+    outfile = 'cal.ics'
+    convert_ics(infile, outfile)
