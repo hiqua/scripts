ba3caed3f06ba8246081054a23440fc67f8a4637
Quentin Hibon <qh.public@yahoo.com>
2018-02-02 17:49:20 +0100

    copy_playing_file

---
+++ copy_playing_file.py
@@ -0,0 +1,52 @@
+import os
+import re
+from pathlib import Path
+import shutil
+
+import mpd
+
+
+def get_client(addr, port, password=None):
+    client = mpd.MPDClient(use_unicode=True)
+    client.connect(addr, port)
+
+    if password is not None:
+        client.password(password)
+
+    return client
+
+
+def playing_file_rel(client):
+    rel_path = client.currentsong()['file']
+    return rel_path
+
+
+def music_root(home):
+    with open(home + '/.config/mpd/mpd.conf') as fs:
+        mpd_conf = fs.readlines()
+
+    for line in mpd_conf:
+        if re.match('^music_directory', line):
+            return Path(re.search('".+"', line).group()[1:-1])
+    else:
+        raise KeyError
+
+
+def main(tgt=Path('music/from_mpd')):
+    home = os.getenv("HOME")
+    mobile_dir = os.getenv("MOBILE_DIR")
+    host = os.getenv("MPD_HOST").split("@")
+    port = os.getenv("MPD_PORT")
+    password = host[0] if len(host) == 2 else None
+    addr = host[-1]
+
+    client = get_client(addr, port, password)
+
+    rel_path = playing_file_rel(client)
+
+    root = music_root(home)
+
+    shutil.copy(str(root / rel_path), str(mobile_dir / tgt))
+
+if __name__ == '__main__':
+    main()
