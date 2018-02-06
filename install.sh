853f658c17bec005cdbe2489af016b1720125788
Quentin Hibon <qh.public@yahoo.com>
2018-02-06 10:29:11 +0100

    use safe ln

--- install.sh
+++ install.sh
@@ -3,24 +3,39 @@ set -e
 set -u
 DIRECTORY="$1"
 
+LINK_CMD="ln -i -s"
+safe_link(){
+  if [ -L "$2" ]; then
+    curr="$(readlink -n "$2")"
+    if [ "$curr" = "$1" ]; then
+      # do nothing
+      true
+    else
+      echo "Changing symbolic link from:"
+      echo "$curr"
+      echo "To:"
+      echo "$1"
+      $LINK_CMD "$1" "$2"
+    fi
+  else
+    printf "Creating link: %s\n" "$2"
+    $LINK_CMD "$1" "$2"
+  fi
+  unset curr
+}
+
 cd "$(dirname "$0")"
 
 # if [ ! "$0" = "./$(basename "$0")" ]; then
 #   echo "Execute from base directory"
 #   exit 1
 # fi
-echo "Install the following in $DIRECTORY:"
-for f in *; do
-  if [ ! "$f" = "$(basename "$0")" ]; then
-    echo "$f"
-  fi
-done
+echo "Install in $DIRECTORY:"
 echo "Continue?"
 read -r _
-echo "ok"
 
 for f in *; do
   if [ ! "$f" = "$(basename "$0")" ] && [ -x "$f" ] ; then
-    ln -s -i "$(pwd)""/$f" "$DIRECTORY"
+    safe_link "$(pwd)/$f" "$DIRECTORY/$f"
   fi
 done
