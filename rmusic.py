a4a32dd54f7dd3e84035ae700483bdbfd73ad025
Quentin Hibon <qh.public@yahoo.com>
2019-12-05 23:13:53 +0100

    update rmusic

--- rmusic.py
+++ rmusic.py
@@ -4,24 +4,22 @@ import pathlib
 from pathlib import Path
 
 
-ext = ['flac', 'opus', 'ogg', 'mp3']
-
-
-def child_containing_matching_f(root):
+def child_containing_matching_f(root, ext=('flac', 'opus', 'ogg', 'mp3')):
     candidates = []
     for x in ext:
-        candidates.extend([c.parent for c in root.rglob('*.' + x)])
+        candidates.extend(c.parent for c in root.rglob('*.' + x))
     candidates = list(set(candidates))
     if candidates:
         return random.choice(candidates)
 
 
-def music_root():
-    with open(Path('~/.config/mpd/mpd.conf').expanduser()) as fs:
-        lines = fs.readlines()
-    for l in lines:
-        if l.startswith('music_directory'):
-            return Path(l.split('"')[1]).expanduser()
+def music_root(mpd_conf='~/.config/mpd/mpd.conf'):
+    with open(Path(mpd_conf).expanduser()) as fs:
+        for l in fs:
+            if l.startswith('music_directory'):
+                dir = Path(l.split('"')[1]).expanduser()
+                assert dir.exists()
+                return dir
 
 
 root = music_root()
