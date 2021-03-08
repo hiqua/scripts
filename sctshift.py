9c160e184d0bb20356bb6032aea835ccdb44e02c
Quentin Hibon <qh.public@yahoo.com>
2021-03-08 11:39:33 +0100

    add sctshift.py

---
+++ sctshift.py
@@ -0,0 +1,40 @@
+import subprocess
+from time import localtime
+"""
+A simple wrapper around sct to compute the desired light temperature and set it.
+
+From morning to mid afternoon: high
+mid afternoon to early evening: decrease linearly
+early evening til morning: low
+"""
+LOW_TEMP = 2500
+HIGH_TEMP = 7500
+morningh = 7
+midafterh = 15
+eveningh = 20
+
+def set_temperature(t):
+    command = ["sct", t]
+    subprocess.run(command)
+
+def compute_temperature():
+    tm = localtime()
+    hour = tm.tm_hour
+
+    if morningh <= hour < midafterh:
+        temp = HIGH_TEMP
+    elif midafterh <= hour < eveningh:
+        temp = HIGH_TEMP - (hour - midafterh) / (eveningh - midafterh) * (HIGH_TEMP - LOW_TEMP)
+    else:
+        temp = LOW_TEMP
+
+    return temp
+
+
+
+def main():
+    temp = compute_temperature()
+    set_temperature(str(temp))
+
+if __name__ == "__main__":
+    main()
