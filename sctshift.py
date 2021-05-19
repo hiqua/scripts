d5778bddf22e0fa0b57b1f962b5d476db63ada15
Quentin Hibon <qh.public@yahoo.com>
2021-05-19 18:46:11 +0200

    sctshift: use env variables

--- sctshift.py
+++ sctshift.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
+import os
+
 from time import localtime
 """
 A simple wrapper around sct to compute the desired light temperature and set it.
@@ -10,15 +12,12 @@ early evening til morning: low
 """
 LOW_TEMP = 2500
 HIGH_TEMP = 7500
-morningh = 7
-midafterh = 14
-eveningh = 20
 
 def set_temperature(t):
     command = ["sct", t]
     subprocess.run(command)
 
-def compute_temperature():
+def compute_temperature(morningh, midafterh, eveningh):
     tm = localtime()
     hour = tm.tm_hour
     minutes = tm.tm_min
@@ -35,7 +34,11 @@ def compute_temperature():
 
 
 def main():
-    temp = compute_temperature()
+    morningh = int(os.environ.get('REDSHIFT_MORNING', 8))
+    midafterh = int(os.environ.get('REDSHIFT_MIDAFTER', 17))
+    eveningh = int(os.environ.get('REDSHIFT_EVENING', 21))
+
+    temp = compute_temperature(morningh, midafterh, eveningh)
     set_temperature(str(temp))
 
 if __name__ == "__main__":
