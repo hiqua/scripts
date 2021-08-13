f6d9cf877719c784db1729cd12c2855ae76f64dd
Quentin Hibon <qh.public@yahoo.com>
2021-08-13 21:43:30 +0200

    add rmusic2.py

---
+++ rmusic2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import sys
+import random
+from pathlib import Path
+"""
+Random music using mpd to avoid slow network access.
+"""
+
+
+
+
+def mpd_ls(path):
+    res = subprocess.run(['mpc', 'ls',path], capture_output=True, check=True)
+    return res.stdout.decode('utf-8').split('\n')[:-1]
+
+
+if __name__ == '__main__':
+    root = Path(os.environ['HOME'] + '/90_99_entertainment/01_music')
+    mpd_root = 'musique'
+    artist_folders = mpd_ls(mpd_root)
+    artist_path = random.choice(artist_folders)
+    album_paths = mpd_ls(artist_path)
+    album = root / random.choice(album_paths)
+    if album.exists():
+        print(album)
+    else:
+        sys.exit(1)
